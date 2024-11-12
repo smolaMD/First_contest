4cf3358d8b0180969c80a99206132dfa8d07b973
Смола Максим Дмитриевич <smola.md@phystech.edu>
2024-11-12 19:39:38 +0300

    Create E.cpp

---
+++ Algorithms/E.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+void PrintNum(const int &n) {
+  std::cout << n << " ";
+}
+
+void Push(int n);
+
+void Pop(int n) {
+  if (n == 0) {
+    return;
+  }
+  Push(n - 1);
+  PrintNum(-n);
+  Pop(n - 1);
+}
+
+void Push(int n) {
+  if (n == 0) {
+    return;
+  }
+  Push(n - 1);
+  PrintNum(n);
+  Pop(n - 1);
+}
+
+void MakeLine(int n) {
+  if (n <= 0) {
+    return;
+  }
+  Push(n - 1);
+  PrintNum(n);
+  MakeLine(n - 2);
+}
+
+int main() {
+
+  int n = 0;
+  std::cin >> n;
+  MakeLine(n);
+  return 0;
+}
